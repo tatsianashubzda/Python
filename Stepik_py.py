9964ac59091532c44e0920b6e7c001ad85c38314
tanya_s <tanya_s@bk.ru>
2023-02-20 23:38:19 +0300

    upd Stepik_py

--- Stepik_py.py
+++ Stepik_py.py
@@ -4046,3 +4046,23 @@ print(convert_to_python_case(txt))
 
 
 #--132
+#Середина отрезка
+#Напишите функцию get_middle_point(x1, y1, x2, y2), которая принимает в качестве аргументов координаты концов отрезка 
+#и находит координаты середины отрезка
+
+# объявление функции
+def get_middle_point(x1, y1, x2, y2):
+    x = (x1 + x2) / 2
+    y = (y1 + y2) / 2
+    return x, y
+# считываем данные
+x_1, y_1 = int(input()), int(input())
+x_2, y_2 = int(input()), int(input())
+
+# вызываем функцию
+x, y = get_middle_point(x_1, y_1, x_2, y_2)
+print(x, y)
+
+
+
+#--133
